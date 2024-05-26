914ac83c03f9e7aa89983a3b59f6d9fcdb4442b0
furqan-qadri <furqaanqadri@gmail.com>
2024-05-27 00:46:29 +0800

    made ngrok link to test

--- tests/headings.spec.ts
+++ tests/headings.spec.ts
@@ -5,7 +5,7 @@ test("should display headings and show alert when button is clicked", async ({
   page,
 }) => {
   // Navigate to the page
-  await page.goto("http://localhost:3000"); // Adjust URL if needed
+  await page.goto("https://powerful-trusted-kodiak.ngrok-free.app/"); // Adjust URL if needed
 
   // Assert the headings are displayed
   const h1 = await page.locator("h1").innerText();
@@ -13,7 +13,7 @@ test("should display headings and show alert when button is clicked", async ({
   const h3 = await page.locator("h3").innerText();
 
   expect(h1).toBe("Hello");
-  
+
   expect(h2).toBe("My Name");
   expect(h3).toBe("Furqan");
 });
